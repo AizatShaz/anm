8c55e2a8f048339a63ce8f4d032f341d88ee9770
AizatShaz <92089781+AizatShaz@users.noreply.github.com>
2022-02-21 14:55:03 +0800

    TTTTTTT

--- resources/views/jualan/kad_kerja/fabrication.blade.php
+++ resources/views/jualan/kad_kerja/fabrication.blade.php
@@ -442,14 +442,721 @@
 <label for="">KETERANGAN:</label>
 <textarea name="" id="" cols="30" rows="10"></textarea><br>
 <br>
-
-
-
-
-
-
-
-
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>TARIKH</th>
+            <th>KETERANGAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>23-21-2021</td>
+            <td>4</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<br>
+<h5>PROSES FABRIKASI SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>REKABENTUK</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>FABRIKASI</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>SENGGARAAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>PEMERIKSAAN</td>
+            <td>"NAMA"</td>
+            <td>2</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>6.</td>
+            <td>PENGUJIAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>7.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h4>SENGGARAN</h4>
+<br>
+<label for="">NO KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<br>
+<label for="">TARIKH:</label>
+<input type="date" name="" id="">
+<br>
+<br>
+<label for="">KETERANGAN:</label>
+<textarea name="" id="" cols="30" rows="10"></textarea><br>
+<br>
+<button>TAMBAH</button>
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>TARIKH</th>
+            <th>KETERANGAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>13-21-2021</td>
+            <td>3</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<br>
+<h5>PROSES SENGGARAN SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>REKABENTUK</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>FABRIKASI</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>SENGGARAAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>PEMERIKSAAN</td>
+            <td>"NAMA"</td>
+            <td>2</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>6.</td>
+            <td>PENGUJIAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>7.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<h4>PEMERIKSAAN</h4>
+<br>
+<button>TAMBAH MAKLUMAT</button>
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>KERJA ALAT/KOMPONEN</th>
+            <th>KEAADAAN</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>"DATA"</td>
+            <td>"DATA"</td>
+            <td>4</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>TAMBAH</button>
+<br>
+<!-- PAGE -->
+<h4>PEMERIKSAAN</h4>
+<br>
+<button>TAMBAH MAKLUMAT</button>
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>KERJA ALAT/KOMPONEN</th>
+            <th>KEAADAAN</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>"DATA"</td>
+            <td>"DATA"</td>
+            <td>4</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>KEMASKINI</button>
+<br>
+<br>
+<h5>PROSES PEMERIKSAAN SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>REKABENTUK</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>FABRIKASI</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>SENGGARAAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>PEMERIKSAAN</td>
+            <td>"NAMA"</td>
+            <td>2</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>6.</td>
+            <td>PENGUJIAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>7.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<h4>PENGUJIAN</h4>
+<br>
+<button>TAMBAH MAKLUMAT</button>
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>KERJA OPERASI</th>
+            <th>KEAADAAN</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>"DATA"</td>
+            <td>"DATA"</td>
+            <td>4</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>TAMBAH</button>
+<br>
+<!-- PAGE -->
+<h4>PENGUJIAN</h4>
+<br>
+<button>TAMBAH MAKLUMAT</button>
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>KERJA OPERASI</th>
+            <th>KEAADAAN</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>"DATA"</td>
+            <td>"DATA"</td>
+            <td>4</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>KEMASKINI</button>
+<br>
+<br>
+<h5>PROSES PENGUJIAN SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>REKABENTUK</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>FABRIKASI</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>SENGGARAAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>PEMERIKSAAN</td>
+            <td>"NAMA"</td>
+            <td>2</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>6.</td>
+            <td>PENGUJIAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>7.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h4>KERJA SELESAI</h4>
+<br>
+<label for="">NO KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<br>
+<label for="">TARIKH:</label>
+<input type="date" name="" id="">
+<br>
+<br>
+<label for="">KETERANGAN:</label>
+<textarea name="" id="" cols="30" rows="10"></textarea><br>
+<br>
+<button>
+    TAMBAH
+</button>
+<br>
+<!-- PAGE -->
+<br>
+<h4>KERJA SELESAI</h4>
+<br>
+<label for="">NO KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<br>
+<label for="">TARIKH:</label>
+<input type="date" name="" id="">
+<br>
+<br>
+<label for="">KETERANGAN:</label>
+<textarea name="" id="" cols="30" rows="10"></textarea><br>
+<br>
+<button>TAMBAH</button>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>TARIKH</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>03-11-2021</td>
+            <td>SS</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br><br>
+<h5>PROSES KERJA SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>REKABENTUK</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>FABRIKASI</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>SENGGARAAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>PEMERIKSAAN</td>
+            <td>"NAMA"</td>
+            <td>2</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>6.</td>
+            <td>PENGUJIAN</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>7.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
 
 
 
--- resources/views/jualan/kad_kerja/testing.blade.php
+++ resources/views/jualan/kad_kerja/testing.blade.php
@@ -2,5 +2,454 @@
 @extends('bases')
 
 @section('content')   
+<h1>KAD KERJA - PERSONAL DOSIMETER</h1>
 
+<form action="">
+    @csrf
+    <h5>PEMBAHAGIAN TUGAS</h5>
+
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>TERIMA PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>LAPORAN DOS</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>KASET LENCANA</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<h4>PROSES TERIMAAN PERMOHONAN</h4>
+<h2>FILM HANDLING : REPORT OF FILM SENT</h2>
+<label for="">CID:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">YEAR/MONTH:</label>
+<input type="month" name="" id="">
+<br>
+<button>ESSDL</button>
+<br>
+<br>
+<h5>PROSES TERIMAAN PERMOHONAN SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<h5>PEMBAHAGIAN TUGAS</h5>
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>TERIMA PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>LAPORAN DOS</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>KASET LENCANA</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h4>PROSES LAPORAN DOS</h4>
+<label for="">CID:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">YEAR/MONTH:</label>
+<input type="month" name="" id="">
+<br>
+<button>ESSDL</button>
+<br>
+<label for="">NAME:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">MONTH/YEAR:</label>
+<input type="month" name="" id="">
+<br>
+<label for="">OD CONTROL(P):</label>
+<input type="text" name="" id="">
+<br>
+<label for="">OD CONTROL(TP):</label>
+<input type="text" name="" id="">
+<br>
+<label for="">OD READ:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">RESULT:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">STATUS:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<h5>PROSES LAPORAN DOS SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<h5>PEMBAHAGIAN TUGAS</h5>
+
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>TERIMA PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>LAPORAN DOS</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>KASET LENCANA</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!-- PAGE -->
+<br>
+<h4>KASET LENCANA</h4>
+<label for="">NO KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<button>eSSDL</button>
+<br>
+<h5>SENARAI PENERIMA KASET</h5>
+<label for="">RECEIVER NAME:</label>
+<input type="text" name="" id="">
+<br>
+<button>CARIAN</button>
+<br>
+<label for="">TARIKH:</label>
+<input type="date" name="" id="">
+<br>
+<label for="">KUANTITI:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">PENERIMA:</label>
+<input type="text" name="" id="">
+<br>
+<label for=""> CATATAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<h5>PROSES KASET LENCANA SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<!-- PAGE -->
+<h5>PEMBAHAGIAN TUGAS</h5>
+
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>TERIMA PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>LAPORAN DOS</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>KASET LENCANA</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+<!— KERJA SELESAI PAGE -->
+<br>
+<h4>KERJA SELESAI</h4>
+<br>
+<label for="">NO KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<br>
+<label for="">TARIKH:</label>
+<input type="date" name="" id="">
+<br>
+<br>
+<label for="">KETERANGAN:</label>
+<textarea name="" id="" cols="30" rows="10"></textarea><br>
+<br>
+<button>TAMBAH</button>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th>BIL</th>
+            <th>TARIKH</th>
+            <th>CATATAN</th>
+            <th>TINDAKAN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>03-11-2021</td>
+            <td>SS</td>
+            <td>
+                 <i class="fas fa-pen"><button></button></i>
+                <i class="far fa-trash-alt"><button></button></i>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br><br>
+<h5>PROSES KERJA SELESAI</h5>
+<br>
+<label for=“”>TARIKH SIAP :</label>
+<input type="date" name="" id="">
+<br>
+<button>SELESAI</button>
+<br>
+<!-- PAGE -->
+<h5>PEMBAHAGIAN TUGAS</h5>
+
+<br>
+<br>
+<label for="">TARIKH JANA KAD KERJA:</label>
+<input type="text" name="" id="">
+<br>
+<label for="">TARIKH PEMBAHAGIAN:</label>
+<input type="text" name="" id="">
+<br>
+<br>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>BIL.</th>
+            <th>PROSES</th>
+            <th>STAF</th>
+            <th>TEMPOH(HARI) SEBENAR/JANGKA</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td>TERIMA PERMOHONAN </td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>LAPORAN DOS</td>
+            <td>"NAMA"</td>
+            <td>4</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>KASET LENCANA</td>
+            <td>"NAMA"</td>
+            <td>5</td>
+            <td>X
+            </td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>KERJA SELESAI</td>
+            <td>"NAMA"</td>
+            <td>3</td>
+            <td>X
+            </td>
+        </tr>
+    </tbody>
+</table>
+<br>
+<button>PAPAR AKTIVITI</button>
+<br>
+
+</form>
 @endsection

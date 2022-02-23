13137e4de901bbc9016b0e25f04d705b2e1c949d
AizatShaz <92089781+AizatShaz@users.noreply.github.com>
2022-02-23 10:05:29 +0800

    Update profil_syarikat.blade.php

--- resources/views/jualan/kod/profil_syarikat.blade.php
+++ resources/views/jualan/kod/profil_syarikat.blade.php
@@ -2,6 +2,8 @@
 @extends('bases')
 
 @section('content')   
+<h1>KOD PROFIL SYARIKAT</h1>
+<h1>CARIAN PROFIL SYARIKAT</h1>
 <form action="">
     @csrf
     <br>
@@ -23,6 +25,91 @@
     <button>CARIAN</button>
     <br>
     <button>TAMBAH SYARIKAT BARU</button>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <table>
+        <thead>
+            <tr>
+                <th>BIL</th>
+                <th>NAMA</th>
+                <th>NO. SYARIKAT</th>
+                <th>TINDAKAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1.</td>
+                <td>""</td>
+                <td>""</td>
+                <td>
+                    <button><i class="far fa-pen"></i></button>
+                    <button> <i class="far fa-trash-alt"></i></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <!--PAGES-->
+</form>
+<form action="">
+    @csrf
+<h1>TAMBAH PROFIL SYARIKAT</h1>
+    <br>
+    <br>
+    <br>
+    <label for="">NO SYARIKAT:</label>
+    <input type="text" name="" id="">
+    <br>
+    <label for="">KATEGORI SYARIKAT:</label>
+    <select name="" id="">
+        <option value="">SILA PILIH</option>
+    </select>
+    <br>
+    <br>
+    
+    <br>
+    <label for="">NAMA SYARIKAT:</label>
+    <input type="text" name="" id="">
 
+
+    <br>
+    <button>CARIAN</button>
+    <br>
+    <button>TAMBAH SYARIKAT BARU</button>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <table>
+        <thead>
+            <tr>
+                <th>BIL</th>
+                <th>NAMA</th>
+                <th>NO. SYARIKAT</th>
+                <th>TINDAKAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1.</td>
+                <td>""</td>
+                <td>""</td>
+                <td>
+                    <button><i class="far fa-pen"></i></button>
+                    <button> <i class="far fa-trash-alt"></i></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <!--PAGES-->
 </form>
 @endsection
